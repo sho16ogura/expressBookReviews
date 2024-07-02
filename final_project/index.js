17611a9e8784b733334b3b2de6fab533e87cc818
sho16ogura <sho16_og@icloud.com>
2024-07-02 03:52:13 -0400

    added REST API

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-
+    
     if (req.session.authorization) {
         let token = req.session.authorization['accessToken'];
 
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,38 +6,115 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (username && password) {
+        // Check if the user does not already exist
+        if (isValid(username)) {
+            // Add the new user to the users array
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "User successfully registered. Now you can login"});
+        } else {
+            return res.status(404).json({message: "Invalid user name. Please use different one"});
+        }
+    }
+    // Return error if username or password is missing
+    return res.status(404).json({message: "Unable to register user."});
+
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', (req, res) => {
+
+    res.status(200).send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+    const isbn = req.params.isbn;
+    let book = books[isbn];
+
+    if (book){
+
+        res.status(200).send(books[isbn]);
+        
+    } else{
+
+        res.status(404).send(`Cannot find book with isbn ${isbn}`);
+
+    }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+    const author = req.params.author;
+    let booksByAuthor = []
+
+    for (isbn of Object.keys(books)){
+
+        let book = books[isbn];
+
+        if (author == book.author){
+
+            booksByAuthor.push({
+                "isbn":isbn,
+                "title":book.title,
+                "reviews":book.reviews
+            })
+        }
+    }
+
+    if (booksByAuthor.length > 0){
+        res.status(200).send(booksByAuthor);
+    }else{
+        res.status(404).send(`Cannot find book written by author ${author}`)
+    }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+    const title = req.params.title;
+    let booksByTitle = []
+
+    for (isbn of Object.keys(books)){
+
+        let book = books[isbn];
+
+        if (title == book.title){
+
+            booksByTitle.push({
+                "isbn":isbn,
+                "author":book.author,
+                "reviews":book.reviews
+            })
+        }
+    }
+
+    if (booksByTitle.length > 0){
+        res.status(200).send(booksByTitle);
+    }else{
+        res.status(404).send(`Cannot find book titled ${title}`)
+    }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    let book = books[isbn];
+
+    if (book){
+
+        res.status(200).send({"reviews": book.reviews});
+        
+    } else{
+
+        res.status(404).send(`Cannot find book with isbn ${isbn}`);
+
+    }
 });
 
 module.exports.general = public_users;
